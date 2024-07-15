df5ed4e1cf7d876772c2d78a167be29266d28e84
Phred Lane <planeiii@thousandeyes.com>
2024-07-15 08:10:03 -0500

    added yaml-companion

---
+++ lua/plugins/yaml-companion.lua
@@ -0,0 +1,12 @@
+-- YAML schema helper
+return {
+  'someone-stole-my-name/yaml-companion.nvim',
+  requires = {
+    { 'neovim/nvim-lspconfig' },
+    { 'nvim-lua/plenary.nvim' },
+    { 'nvim-telescope/telescope.nvim' },
+  },
+  config = function()
+    require('telescope').load_extension('yaml_schema')
+  end,
+}
