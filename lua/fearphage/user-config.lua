d1633831387f8b0d4f1c3d2aae41e93184e3cee3
Phred <fearphage@gmail.com>
2024-07-17 05:27:05 -0500

    cleaned up config

--- lua/fearphage/user-config.lua
+++ lua/fearphage/user-config.lua
@@ -2,32 +2,6 @@
 
 local M = {}
 
--- add any null-ls sources you want here
-M.setup_sources = function(b)
-  return {
-    b.formatting.autopep8,
-    b.code_actions.gitsigns,
-  }
-end
-
--- add null_ls sources to auto-install
-M.null_ls_ensure_installed = {
-  'stylua',
-  'jq',
-}
-
--- add servers to be used for auto formatting here
-M.formatting_servers = {
-  ['rust_analyzer'] = { 'rust' },
-  ['lua_ls'] = { 'lua' },
-  ['null_ls'] = {
-    'javascript',
-    'javascriptreact',
-    'typescript',
-    'typescriptreact',
-  },
-}
-
 -- options you put here will override or add on to the default options
 M.options = {
   opt = {
@@ -41,7 +15,6 @@ M.autocommands = {
   inlay_hints = true,
   alpha_folding = true,
   treesitter_folds = true,
-  trailing_whitespace = true,
   remember_file_state = true,
   session_saved_notification = true,
   format_on_autosave = true,
@@ -74,7 +47,6 @@ M.enable_plugins = {
   neotree = true,
   noice = true,
   notify = true,
-  null_ls = true,
   onedark = true,
   project = true,
   scope = true,
@@ -158,7 +130,7 @@ M.lsp_servers = {
       },
     },
   },
-  -- nil_ls = {}, -- nix
+  nil_ls = {}, -- nix
   pyright = {},
   -- remark_ls = {}, -- markdown
   svelte = {},
